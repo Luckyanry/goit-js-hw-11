23da5dddc63bde14a1ad2813ff4fe5071d178c60
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-13 20:47:27 +0200

    Finished task with color-switch of goit-js-hw-11

--- color-switch/switch.js
+++ color-switch/switch.js
@@ -10,8 +10,32 @@
     Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
 */
 
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body"),
+};
+
 const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"];
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+refs.start.addEventListener("click", changeStart);
+
+const randomizer = () => {
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  let randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+
+  refs.body.style.backgroundColor = `${colors[randomIndex]}`;
 };
+
+function changeStart() {
+  const intervalId = setInterval(randomizer, 1000);
+
+  refs.stop.addEventListener("click", changeStop);
+
+  function changeStop() {
+    clearInterval(intervalId);
+  }
+}
