ef1fbb66616e60540f340cd5fd0804c018a134ab
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-14 19:06:43 +0200

    Realized timer using class CountdownTimer of js-hw-11-timer

--- timer/timer.js
+++ timer/timer.js
@@ -8,53 +8,95 @@
     Для подсчета значений используй готовые формулы, где time - разница между targetDate и текущей датой.
 */
 
-// new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Jul 17, 2019"),
-// });
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
 
-const refs = {
-  clockface: document.querySelector("#timer-1"),
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  secs: document.querySelector('span[data-value="secs"]'),
-};
+    this.start();
+  }
 
-const timer = {
   start() {
-    const targetDate = new Date("Jun 20, 2020 18:00:00").getTime();
+    const targetDate = this.targetDate.getTime();
 
     const timeIsOut = setInterval(() => {
       const currentDate = new Date().getTime();
       const time = targetDate - currentDate;
 
-      updateClockface(time, timeIsOut);
+      this.updateClockface(time, timeIsOut);
     }, 1000);
-  },
-};
+  }
 
-timer.start();
+  updateClockface(time, timeIsOut) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-function updateClockface(time, timeIsOut) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    this.selector.textContent = `${days}:${hours}:${mins}:${secs}`;
 
-  // refs.days.textContent = days;
-  // refs.hours.textContent = hours;
-  // refs.mins.textContent = mins;
-  // refs.secs.textContent = secs;
-  refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+    if (time < 0) {
+      clearInterval(timeIsOut);
+      this.selector.textContent = "Your time is out!!!";
+    }
+  }
 
-  if (time < 0) {
-    clearInterval(timeIsOut);
-
-    refs.clockface.textContent = "Your time is out!!!";
+  pad(value) {
+    return String(value).padStart(2, "0");
   }
 }
 
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jun 20, 2020 18:00:00"),
+});
+
+//
+//
+
+// ====================================================================
+// const refs = {
+//   clockface: document.querySelector("#timer-1"),
+//   days: document.querySelector('span[data-value="days"]'),
+//   hours: document.querySelector('span[data-value="hours"]'),
+//   mins: document.querySelector('span[data-value="mins"]'),
+//   secs: document.querySelector('span[data-value="secs"]'),
+// };
+
+// const timer = {
+//   start() {
+//     const targetDate = new Date("Jun 20, 2020 18:00:00").getTime();
+
+//     const timeIsOut = setInterval(() => {
+//       const currentDate = new Date().getTime();
+//       const time = targetDate - currentDate;
+
+//       updateClockface(time, timeIsOut);
+//     }, 1000);
+//   },
+// };
+
+// timer.start();
+
+// function updateClockface(time, timeIsOut) {
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+// refs.days.textContent = days;
+// refs.hours.textContent = hours;
+// refs.mins.textContent = mins;
+// refs.secs.textContent = secs;
+// // refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
+
+//   if (time < 0) {
+//     clearInterval(timeIsOut);
+
+//     refs.clockface.textContent = "Your time is out!!!";
+//   }
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, "0");
+// }
