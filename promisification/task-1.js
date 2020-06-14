200fd8dae183bb50875ff698f3a3fd55c10ade1b
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-14 20:46:02 +0200

    Shortened the promise record on task-1 =)

--- promisification/task-1.js
+++ promisification/task-1.js
@@ -5,9 +5,7 @@
 
 const delay = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`${ms}`);
-    }, ms);
+    setTimeout(() => resolve(`${ms}`), ms);
   });
 };
 
