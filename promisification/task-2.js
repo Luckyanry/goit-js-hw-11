805455c4856ab7c5342243fcca9bd38a63e04061
Andrii Kochmaruk <luckyanry@gmail.com>
2020-06-15 00:55:31 +0200

    Changed Promise.resolve on Promise.all in task-2 of js-hw-11-promosification

--- promisification/task-2.js
+++ promisification/task-2.js
@@ -13,7 +13,7 @@ const users = [
 const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map((user) => (user.name === userName ? { ...user, active: !user.active } : user));
 
-  return Promise.resolve(updatedUsers);
+  return Promise.all(updatedUsers);
 };
 
 const logger = (updatedUsers) => console.table(updatedUsers);
